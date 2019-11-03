8cd52820af5243912cbfacb3602709f451aa238b
Kim Skinner <kim@kjdesigns.com.au>
2019-11-03 16:11:10 +0100

    Added my own paths

--- path.zsh
+++ path.zsh
@@ -11,3 +11,15 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
 #export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+
+# Load the Laravel Spark installer
+export PATH=/Users/kimskinner/Packages/spark-installer:$PATH
+
+# This is unknown, may need to enable it if I have issues
+# export PATH="/usr/local/sbin:$PATH"
+
+# Needed for a brew install, but may not need it
+# export PATH="/usr/local/opt/gettext/bin:$PATH"
+
+# Load Visual Studio Code to enable quick launching
+export PATH="/Applications/Visual Studio Code.app/Contents/Resources/app/bin:$PATH"
